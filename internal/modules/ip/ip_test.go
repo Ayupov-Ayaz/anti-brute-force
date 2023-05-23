794d9da88a78297ce32d17d15943e4937dbcff53
Ayaz Ayupov <ayaz@releaseband.com>
2023-05-23 20:39:32 +0700

    add unit tests

--- internal/modules/ip/ip_test.go
+++ internal/modules/ip/ip_test.go
@@ -1,6 +1,7 @@
 package ip
 
 import (
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -28,6 +29,30 @@ func TestJoinIPMask(t *testing.T) {
 				require.NoError(t, err)
 			}
 			require.Equal(t, tt.expected, ipMasked.String())
+		})
+	}
+}
+
+func TestIPToUint32(t *testing.T) {
+	tests := []struct {
+		ip       string
+		expected uint32
+	}{
+		{
+			ip:       "64.233.187.99",
+			expected: 1089059683,
+		},
+		{
+			ip:       "255.255.255.254",
+			expected: 4294967294,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.ip, func(t *testing.T) {
+			ip := net.ParseIP(tt.ip)
+			require.NotZero(t, ip)
+			require.Equal(t, tt.expected, ipToUint32(ip))
 		})
 	}
 }
